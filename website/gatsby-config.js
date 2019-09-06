9417f333ecfb790fade038a4ea2c321d61f82f04
sarpisik <contact@sarpisik.com>
2019-09-06 20:30:24 +0300

    Edit api url

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: "http://localhost:1337",
+        apiURL: process.env.API_URL || "http://localhost:1337",
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
           "project",
