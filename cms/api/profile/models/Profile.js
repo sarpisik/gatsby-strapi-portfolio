4d11247b46483644485948cccb58caa4da5d3053
sarpisik <contact@sarpisik.com>
2019-10-03 21:53:34 +0300

    Add disable hooks

--- cms/api/profile/models/Profile.js
+++ cms/api/profile/models/Profile.js
@@ -8,47 +8,36 @@ module.exports = {
   // Before saving a value.
   // Fired before an `insert` or `update` query.
   // beforeSave: async (model, attrs, options) => {},
-
   // After saving a value.
   // Fired after an `insert` or `update` query.
   // afterSave: async (model, attrs, options, result) => {},
-
   // Before fetching all values.
   // Fired before a `fetchAll` operation.
   // beforeFetchAll: async (model, attrs, options) => {},
-
   // After fetching all values.
   // Fired after a `fetchAll` operation.
   // afterFetchAll: async (model, attrs, options, results) => {},
-
   // Fired before a `fetch` operation.
   // beforeFetch: async (model, attrs, options) => {},
-
   // After fetching a value.
   // Fired after a `fetch` operation.
   // afterFetch: async (model, attrs, options, result) => {},
-
   // Before creating a value.
   // Fired before an `insert` query.
   // beforeCreate: async (model, attrs, options) => {},
-
   // After creating a value.
   // Fired after an `insert` query.
-  afterCreate: async () => await strapi.services.website.build(),
-
+  // afterCreate: async () => await strapi.services.website.build(),
   // Before updating a value.
   // Fired before an `update` query.
   // beforeUpdate: async (model, attrs, options) => {},
-
   // After updating a value.
   // Fired after an `update` query.
-  afterUpdate: async () => await strapi.services.website.build(),
-
+  // afterUpdate: async () => await strapi.services.website.build(),
   // Before destroying a value.
   // Fired before a `delete` query.
   // beforeDestroy: async (model, attrs, options) => {},
-
   // After destroying a value.
   // Fired after a `delete` query.
-  afterDestroy: async () => await strapi.services.website.build()
+  // afterDestroy: async () => await strapi.services.website.build()
 };
